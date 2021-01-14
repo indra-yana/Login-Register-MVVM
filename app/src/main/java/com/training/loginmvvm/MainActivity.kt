3abe79506fbf9e8b4e30087e3cabd53d33a562b8
Indra Muliana <indra.ndra26@gmail.com>
2021-01-14 16:22:08 +0700

    Setup navigation view

--- app/src/main/java/com/training/loginmvvm/MainActivity.kt
+++ app/src/main/java/com/training/loginmvvm/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.training.loginmvvm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.training.loginmvvm.auth.AuthActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, AuthActivity::class.java))
+        finish()
     }
 }
